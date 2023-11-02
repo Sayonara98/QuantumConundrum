aa3a462bec62404f75a3b2fb035bfbb88df96e79
Long <hoangphilong050698@gmail.com>
2023-11-03 01:40:46 +0700

    + Simple enemy spawner

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBasicEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefabs, transform.position, Quaternion.identity);
+    }
+}
