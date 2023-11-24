189a22b766f6f4817ea406274e74a2a3d78c3fe8
Doãn Hiệp <33453150+dhiepp@users.noreply.github.com>
2023-11-24 09:07:21 +0700

    A minus wip

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour, IDamageable
@@ -21,24 +24,38 @@ public class Enemy : MonoBehaviour, IDamageable
     private EnemyShield shield;
 
     private float scanTimer = 0.5f;
-    private float reScan = 0.5f;
+    private readonly float reScan = 0.5f;
 
+    private List<Vector3> foundPath = new();
+    private float pathTimer = 2.0f;
+    private readonly float pathResetTime = 2.0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player");
-        
     }
 
     private void Update()
     {
-        if (scanTimer <= 0)
+        if (foundPath.Count > 0)
         {
-            DetectTarget();
-            scanTimer = reScan;
+            if (pathTimer <= 0)
+            {
+                pathTimer = pathResetTime;
+                foundPath.Clear();
+            }
+            pathTimer -= Time.deltaTime;
         }
-        scanTimer -= Time.deltaTime;
+        else
+        {
+            if (scanTimer <= 0)
+            {
+                DetectTarget();
+                scanTimer = reScan;
+            }
+            scanTimer -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -72,9 +89,66 @@ public class Enemy : MonoBehaviour, IDamageable
     {
         if (target)
         {
-            direction = target.transform.position - transform.position;
+            Vector3 current = transform.position;
+            Vector3 destination;
+            if (foundPath.Count == 0)
+            {
+                destination = target.transform.position;
+            }
+            else
+            {
+                destination = foundPath.First();
+            }
+                
+            direction = destination - current;
             direction.Normalize();
+
+            var check = current + new Vector3(direction.x, direction.y);
+            if (MapManager.Instance.GetBiomeType(check) == BiomeType.WATER)
+            {
+                foundPath.AddRange(FindPathAroundWater());
+            }
         }
+    }
+
+    private List<Vector3> FindPathAroundWater()
+    {
+        Vector3 destination = target.transform.position;
+
+        var road = new List<Vector3> { transform.position };
+
+        for (int p = 1 ; p < 100; p++)
+        {
+            Vector3 current = road.Last();
+            if (Vector3.Distance(current, destination) < range)
+            {
+                return road;
+            }
+            
+            float minDistance = float.MaxValue;
+            Vector3 minPos = Vector3.zero;
+        
+            for (int i = 45; i < 360; i += 45)
+            {
+                Vector3 check = Quaternion.AngleAxis(i, Vector3.forward) * Vector3.one + current;
+                Debug.Log(check);
+                var dist = Vector3.Distance(check, destination);
+                if (MapManager.Instance.CheckPassable(check) && dist < minDistance)
+                {
+                    minDistance = dist;
+                    minPos = check;
+                }
+            }
+
+            if (minPos == Vector3.zero || Vector3.Distance(minPos, destination) < range)
+            {
+                return road;
+            }
+            road.Add(minPos);
+            Debug.DrawLine(current, minPos);
+        }
+
+        return road;
     }
 
     bool IsTargetInRange()
